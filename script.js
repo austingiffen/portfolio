75f429b85a39e8f5e13b35dbef6c4c61dcb47e83
Austin Giffen <24austin.giffen@gmail.com>
2025-09-07 16:47:07 -0400

    h

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 function toggleMenu() {
   const sidebar = document.getElementById("sidebar");
+  const hamburger = document.querySelector(".hamburger");
+
   sidebar.classList.toggle("active");
+  hamburger.classList.toggle("active");
 }
